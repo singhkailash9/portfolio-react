548bd44e27e1bb46c907b31b830a1f1724b15a72
Kailash Singh <singhkailash.sk9@gmail.com>
2024-02-01 16:54:02 +0530

    Update Header and Navbar

--- src/components/Header Footer/Header.js
+++ src/components/Header Footer/Header.js
@@ -7,7 +7,26 @@ const Header = () => {
     if (section) {
       section.scrollIntoView({ behavior: 'smooth' });
     }
+    if (window.innerWidth <= 670) {
+      if (sectionId === 'home' || sectionId === 'projects' || sectionId === 'skills') {
+        return;
+      } else {
+        toggleNavbar();
+      }
+    }
   };
+
+  function toggleNavbar() {
+    if (window.innerWidth > 670){
+      return;
+    }
+    const collapsibleNavbar = document.getElementById('collapsible-navbar');
+    if (collapsibleNavbar.style.display === "block") {
+      collapsibleNavbar.style.display = "none";
+    } else {
+      collapsibleNavbar.style.display = "block";
+    }
+  }
 
   return (
     <header>
@@ -15,27 +34,23 @@ const Header = () => {
         <div id='header-name'>
           Kailash
         </div>
-        <div>
-          <ul className='Navbar'>
-            <li>
-              <button onClick={() => scrollToSection('home')}>Home</button>
-            </li>
-            <li>
-              <button onClick={() => scrollToSection('projects')}>Projects</button>
-            </li>
-            <li>
-              <button onClick={() => scrollToSection('skills')}>Skills</button>
-            </li>
-            <li>
-              <button onClick={() => scrollToSection('about')}>About</button>
-            </li>
-            <li>
-              <button onClick={() => scrollToSection('contact')}>Contact</button>
-            </li>
-            <li>
-              <a href="https://drive.google.com/drive/folders/13WcfQSwX7onZpT90ZQ9yVQkrxfkjgUh5?usp=sharing" 
-              target="_blank" rel="noopener noreferrer" id="resume-link">Resume</a>
-            </li>
+        <div id='navbar-container'>
+          <div id='first-half'>
+            <ul className='nav-list'>
+              <li><button onClick={() => scrollToSection('home')}>Home</button></li>
+              <li><button onClick={() => scrollToSection('projects')}>Projects</button></li>
+              <li><button onClick={() => scrollToSection('skills')}>Skills</button></li>
+            </ul>
+            <button id="toggle-button" onClick={toggleNavbar}>
+              <img id='nav-image' src='/icons/navbar.png' alt='navbar'></img>
+            </button>
+          </div>
+          <ul className='nav-list' id='collapsible-navbar'>
+            <li><button onClick={() => scrollToSection('about')}>About</button></li>
+            <li><button onClick={() => scrollToSection('contact')}>Contact</button></li>
+            <li><a target="_blank" rel="noopener noreferrer" id="resume-link" onClick={toggleNavbar}
+              href="https://www.canva.com/design/DAF7YGbdEAQ/u4Ia4yMxbjIK5CchjInDBQ/view?utm_content=DAF7YGbdEAQ&utm_campaign=designshare&utm_medium=link&utm_source=editor">
+              Resume</a></li>
           </ul>
         </div>
       </div>
